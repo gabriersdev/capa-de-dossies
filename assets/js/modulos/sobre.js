830779530ec989d6ae3e12056765e841624e5c50
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2024-04-11 22:36:16 -0300

    Informações do projeto e estruturação das features

--- assets/js/modulos/sobre.js
+++ assets/js/modulos/sobre.js
@@ -17,13 +17,22 @@ let dados_do_projeto = {
   Status: 'Active',
 };
 
-const novas_funcionalidades = [
+const novas_funcionalidades = {
   // Lista de novas funcionalidades
-  'Implementado criação de capa através de espelho de proposta do SIOPI',
-  'Corrigida falha na recuperação das capas armazenadas localmente',
-  'Iniciada a implementação de link para compartilhamento da capa',
-  'Arquivos não utilizados foram removidos',
-];
+  "3.0.0": [
+    'Implementado criação de capa através de espelho de proposta do SIOPI',
+    'Corrigida falha na recuperação das capas armazenadas localmente',
+    'Iniciada a implementação de link para compartilhamento da capa',
+    'Arquivos não utilizados foram removidos',
+  ],
+  "3.1.0": [
+    'Corrigida falhas na criação do título do processo',
+    'Adicionado campo para FGTS futuro',
+    'Adicionado controle de exibição para FGTS futuro',
+    'Implementada verificação de registro duplicados no salvamento das capas',
+  ]
+
+};
 
 // Carregando dados do arquivo de manifest.json
 export default async () => {
@@ -41,10 +50,10 @@ export default async () => {
     };
     dados_do_projeto = (Object.assign({}, dados_manifest, dados_do_projeto));
 
-    Object.freeze(novas_funcionalidades);
+    Object.freeze(novas_funcionalidades[manifest.version]);
     Object.freeze(dados_do_projeto);
 
-    apresentarDadosProjeto(dados_do_projeto, novas_funcionalidades);
+    apresentarDadosProjeto(dados_do_projeto, novas_funcionalidades[manifest.version]);
     return true;
   })
   .catch((error) => {
